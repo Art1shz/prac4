7020ff41881d52b20c2c82932cd4d2d6796afe77
Art1shz <art1shz@mail.ru>
2023-03-16 00:08:09 +0300

    prac4

---
+++ prac4 на 3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] nums = new int[num.ToString().Length];
+int a = 0;
+int b = 1;
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = num % 10;
+    num /= 10;
+}
+Array.Reverse(nums);
+Console.WriteLine(string.Join(" ", nums));
+for (int k = 0; k < nums.Length; k++)
+{
+    a += nums[k]; b *= nums[k];
+}
+int max = nums.Max();
+int min = nums.Min();
+Console.WriteLine($"Сумма чисел: {a}\r\nПроизведение чисел: {b}");
+Console.WriteLine($"Максимальное значение: {max}\r\nМинимальное значение: {min}");
